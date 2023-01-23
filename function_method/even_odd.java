8dea6cf7c4b1fbeabfd39af2f0c898f4e2cdf2c1
shoeb <wiqbal091@gmail.com>
2023-01-23 20:37:07 +0530

    commit success full

--- function_method/even_odd.java
+++ function_method/even_odd.java
@@ -3,17 +3,15 @@ package function_method;
 import java.util.Scanner;
 
 public class even_odd {
-	public static int evenOdd(int n)
+	public static void evenOdd(int n)
 	{
 		if (n%2==0)
 		{
 			System.out.println("Number is Even: ");
-			return 1;
 		}
 		else
 		{
 			System.out.println("Number is Odd: ");
-			return 0;
 		}
 	}
 	
